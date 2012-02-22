69b26c2a402453de1f4dcd7c87009bec23b6a407
Jon Muir <jon@xtreme4s.net>
2012-02-23 04:38:16 +1100

    created an example

---
+++ test/jquery.test.js
@@ -0,0 +1,9 @@
+(function( $ ){
+
+    $.fn.special-carousel = function (settings) {
+        return $(this).each(function () {
+	  // you know what this should do...
+        });
+    };
+
+})(jQuery);
